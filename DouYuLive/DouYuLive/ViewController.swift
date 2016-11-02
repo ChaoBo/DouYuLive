614db69a20b3ad8ca411d8fe5ef1a845f2d48c8e
晁博 <631616788@qq.com>
2016-11-03 01:46:55 +0800

    [Commit User]  ChaoBo <631616788@qq.com> [Commit Message] 初始化项目 [Modified File List] 	new file:   DouYuLive/DouYuLive.xcodeproj/project.pbxproj 	new file:   DouYuLive/DouYuLive.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   DouYuLive/DouYuLive/AppDelegate.swift 	new file:   DouYuLive/DouYuLive/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   DouYuLive/DouYuLive/Base.lproj/LaunchScreen.storyboard 	new file:   DouYuLive/DouYuLive/Base.lproj/Main.storyboard 	new file:   DouYuLive/DouYuLive/Info.plist 	new file:   DouYuLive/DouYuLive/ViewController.swift

---
+++ DouYuLive/DouYuLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouYuLive
+//
+//  Created by 晁博 on 2016/11/3.
+//  Copyright © 2016年 狂虐iOS(中国). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
